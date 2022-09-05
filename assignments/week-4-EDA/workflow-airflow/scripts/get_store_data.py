402328829bce648c4662a454fe9014915d921dd4
swethabez <71044047+swethabez@users.noreply.github.com>
2022-09-05 01:29:57 -0400

    update questions

--- assignments/week-4-EDA/workflow-airflow/scripts/get_store_data.py
+++ assignments/week-4-EDA/workflow-airflow/scripts/get_store_data.py
@@ -7,3 +7,4 @@ data = pd.read_csv(path.joinpath(filename))
 data_store1 = data[data.Store==1]
 ofname = filename.replace('train_comb', 'train-store1')
 data_store1.to_csv(path.joinpath(ofname), index=False)
+
